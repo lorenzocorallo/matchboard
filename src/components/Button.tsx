09cfc8c1fd989682c5c531a4d3b7985e52dc9bb1
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2022-08-03 19:18:22 +0200

    Added Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({ children, className = "", ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+	return (
+		<button
+			className={`m-2 px-4 py-2 bg-black text-white rounded-md hover:bg-gray-700 transition-all ${className}`}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
