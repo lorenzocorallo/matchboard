cb86233933d397752de534407819afac83183793
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2022-08-03 23:29:57 +0200

    Added full height text field

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -2,24 +2,29 @@ import React, { useId } from "react";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 	fullWidth?: boolean;
+	fullHeight?: boolean;
 	label?: string;
 	inline?: boolean;
 }
 
-const TextField = ({ fullWidth, label, inline, className = "", ...props }: Props) => {
+const TextField = (
+	{ fullWidth, fullHeight, label, inline, className = "", ...props }: Props,
+	ref: React.ForwardedRef<HTMLInputElement>
+) => {
 	const id = useId();
 	return (
-		<div className={`flex gap-2 items-center ${inline ? "" : "flex-col"}`}>
+		<div className={`flex gap-2 items-center ${inline ? "" : "flex-col"} ${fullHeight ? "h-full" : ""}`}>
 			{label && <label htmlFor={id}>{label}</label>}
 			<input
+				ref={ref}
 				id={id}
-				className={`${
-					fullWidth ? "w-full" : ""
-				} h-full block p-2 outline-none border-none rounded-md bg-slate-200 dark:bg-slate-500 ${className}`}
+				className={`${fullWidth ? "w-full" : ""} ${
+					fullHeight ? "h-full" : ""
+				} block p-2 outline-none border-none rounded-md bg-slate-200 dark:bg-slate-500 ${className}`}
 				{...props}
 			/>
 		</div>
 	);
 };
 
-export default TextField;
+export default React.forwardRef(TextField);
