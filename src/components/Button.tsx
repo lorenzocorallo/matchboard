4ddd2bb99d6b34243c3c54f68e13a56af3e0cdca
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2022-08-03 22:47:29 +0200

    Added error (red) theme for Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,15 @@
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-	theme?: "success" | "default";
+	theme?: "success" | "error" | "default";
 }
 
 const Button = ({ theme = "default", children, className = "", ...props }: Props) => {
 	return (
 		<button
-			className={`m-2 px-4 py-2 text-white rounded-md  transition-all ${
+			className={`m-2 px-4 py-2 text-white rounded-xl transition-all ${
 				theme === "default" && "bg-gray-600 hover:bg-gray-700"
-			}   ${theme === "success" && "bg-green-600 hover:bg-green-700"} ${className}`}
+			}   ${theme === "success" && "bg-green-600 hover:bg-green-700"} ${
+				theme === "error" && "bg-red-600 hover:bg-red-700"
+			} ${className}`}
 			{...props}
 		>
 			{children}
