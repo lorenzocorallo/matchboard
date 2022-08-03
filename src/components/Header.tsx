fd1fbdc6f16019edcb2e0634494a83d057edd95b
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2022-08-03 23:31:00 +0200

    Styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 
 const Header = ({ title }: Props) => {
 	return (
-		<div className="sticky top-0 w-full bg-white dark:bg-slate-800">
+		<div className="sticky top-0 w-full bg-white dark:bg-slate-800 pt-3">
 			<h1 className="text-3xl">Burraco</h1>
 			{title && <h2 className="text-xl ">{title}</h2>}
 			<hr className="my-3 dark:border-white " />
