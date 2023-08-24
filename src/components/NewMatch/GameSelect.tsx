f8d75688db6d4b130d06368a556cf14b272cf0f4
Lorenzo <lorenzo.corallo@outlook.it>
2023-08-24 16:19:42 +0200

    fix: add key to game select items

--- src/components/NewMatch/GameSelect.tsx
+++ src/components/NewMatch/GameSelect.tsx
@@ -10,7 +10,7 @@ export default function GameSelect () {
       <Header backPath="/" title="Scegli gioco" />
       <div className="flex flex-col items-center gap-4 w-full p-4">
         {GAMES.map((game) => (
-          <Link to={`/new/${game.id}`} className="w-full text-xl">
+          <Link key={game.id} to={`/new/${game.id}`} className="w-full text-xl">
             <Paper>{game.name}</Paper>
           </Link>
         ))}
