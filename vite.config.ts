bbeb0c3f9a4eb504f5867fefa08cab17f3c0e616
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2024-07-21 19:04:56 +0200

    refactor: migrate to vite (#1)
    * migrate to vite
    
    * remove package-lock.json
    
    * specify pnpm as packageManager in package.json
    
    * fix build

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.PUBLIC_URL || "/",
+  build: { outDir: "./dist" },
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
+})
