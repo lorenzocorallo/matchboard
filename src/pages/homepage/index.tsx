f2e795ae2fb9fcb62059cb1178180d7b5e313ed3
Lorenzo <lorenzo.corallo@outlook.it>
2024-07-25 09:08:07 +0200

    feat(homepage): add filter by match finished status

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,24 +1,65 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { MatchesContext } from "@/context/matches";
 import Button from "@/components/button";
 import Header from "@/components/header";
 import Wrapper from "@/components/wrapper";
 import MatchCard from "./match-card";
+import Switch from "@/components/switch";
+
+const MATCH_STATUS = ["all", "finished", "running"] as const;
+type MatchStatusFilter = (typeof MATCH_STATUS)[number];
+
+function getStatusFilterLabel(statusFilter: MatchStatusFilter): string {
+  switch (statusFilter) {
+    case "all":
+      return "Tutte";
+    case "finished":
+      return "Finite";
+    case "running":
+      return "In corso";
+  }
+}
 
 function Matches() {
-  const { matches } = useContext(MatchesContext);
-  return matches.length > 0 ? (
-    <div className="p-4 gap-4 w-full flex flex-col flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-gray-300 scrollbar-thumb-gray-400 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
-      {matches.map((m) => (
-        <MatchCard match={m} key={m.id} />
-      ))}
-    </div>
-  ) : (
-    <div className="grid place-content-center flex-1 max-w-[20rem] gap-4">
-      <p className="text-2xl">
-        Ancora nessuna partita creata, iniziane una ora!
-      </p>
+  const { matches: m } = useContext(MatchesContext);
+  const [selectedStatusFilter, setSelectedStatusFilter] =
+    useState<MatchStatusFilter>("all");
+
+  const matches = m.filter(match => selectedStatusFilter === "all" || match.finished === (selectedStatusFilter === "finished"));
+
+  return (
+    <div className="p-4 gap-4 w-full flex flex-col flex-1 overflow-y-hidden">
+      {matches.length > 0 && (
+        <div className="flex gap-2">
+          {MATCH_STATUS.map((status) => {
+            const label = getStatusFilterLabel(status);
+            return (
+              <Switch
+                key={status}
+                className="py-2 px-4"
+                active={selectedStatusFilter === status}
+                onClick={() => setSelectedStatusFilter(status)}
+                activeLabel={label}
+                inactiveLabel={label}
+              />
+            );
+          })}
+        </div>
+      )}
+      {matches.length > 0 ? (
+        <div className="gap-4 w-full flex flex-col flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-gray-300 scrollbar-thumb-gray-400 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+          {matches.map((m) => (
+            <MatchCard match={m} key={m.id} />
+          ))}
+        </div>
+      ) : (
+        <div className="grid place-content-center flex-1 max-w-[20rem] gap-4">
+          <p className="text-2xl">
+            Ancora nessuna partita creata, iniziane una ora!
+          </p>
+        </div>
+      )}
     </div>
   );
 }
